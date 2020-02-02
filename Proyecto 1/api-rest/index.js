cca1aeae41a0d07faa1a0d9182bbc7f435b3519e
Alejandro <jaromanp@eafit.edu.co>
2020-02-02 10:24:52 -0500

    Cambio de la base de datos local a atlas y adicion del gitignore

--- Proyecto 1/api-rest/index.js
+++ Proyecto 1/api-rest/index.js
@@ -3,6 +3,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const Product = require('./Models/products')
+
+require('dotenv/config')
 
 const app = express()
 const port = process.env.PORT || 3001
@@ -10,17 +13,31 @@ const port = process.env.PORT || 3001
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 
-app.get('/api/resultados', (req , res) => {
+//Rutas
+app.get('/api/consultar', (req , res) => {
     res.send(200, {resultado: []})
 })
 
-app.post('/api/resultados', (req , res) => {
+app.post('/api/enviar_datos', (req , res) => {
+    console.log('POST /api/enviar_datos')
     console.log(req.body)
-    res.status(200).send({message: 'Los datos se han recibido'})
+
+    let product = new product()
+    product.name = req.body.name
+    product.picture = req.body.picture
+    product.price = req.body.price
+    product.category = req.body.category
+    product.description = req.body.description
+
+    product.save((err, productStored) => {
+        if(err) res.status(500).send({message: `Error al guardar en la base de datos ${err}`} )
+
+        res.status(200).send({data: productStored})
+    })
 })
 
-
-mongoose.connect('mongodb://localhost:27017/EventsIoT', (err, res) => {
+//conexion a la base de datos
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true },(err, res) => {
     if(err) {
         return console.log(`Error al conectar a la base de datos: ${err}`)
     }
