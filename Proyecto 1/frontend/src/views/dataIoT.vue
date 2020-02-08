0a3ef2ce6533bf98547771b6642766c508e922c6
Alejandro <jaromanp@eafit.edu.co>
2020-02-08 17:05:01 -0500

    Datos visualizados correctamente

---
+++ Proyecto 1/frontend/src/views/dataIoT.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+        <h1>DataIoT</h1>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Usuario</th>
+      <th scope="col">Temperatura</th>
+      <th scope="col">Humedad</th>
+      <th scope="col">Ubicacion</th>
+      <th scope="col">Fecha </th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(item, index) in datos" :key="index">
+      <td>{{item.userID}}</td>
+      <td>{{item.temperatura}}</td>
+      <td>{{item.humedad}}</td>
+      <td>{{item.ubicacion}}</td>
+      <td>{{item.date}}</td>
+      <td>
+      </td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            datos: []
+        }
+    },
+    created(){
+        this.listarDatos();
+    },
+    methods:{
+        listarDatos(){
+            this.axios.get('/consultar')
+                .then(res =>{
+                    console.log(res.data)
+                    this.datos = res.data
+                })
+                .catch(e => {
+                    console.log(e.response)
+                })
+        }
+    }
+}
+</script>
